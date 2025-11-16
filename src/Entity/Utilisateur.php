e179c901b3b0acd211c3a8891353f0358c40c7b4
MdRiabi <mdriabi@gmail.com>
2025-11-16 03:59:37 +0100

    modification de l'entité utilisateur.php

--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -2,39 +2,122 @@
 
 namespace App\Entity;
 
+use App\Repository\UtilisateurRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
-#[ORM\Entity]
+#[ORM\Entity(repositoryClass: UtilisateurRepository::class)]
+#[ORM\Table(name: 'utilisateur')]
 class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column(type: 'integer')]
+    #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(type: 'string', length: 180, unique: true)]
-    private ?string $email = null;
-
-    #[ORM\Column(type: 'json')]
-    private array $roles = [];
-
-    /**
-     * The hashed password
-     */
-    #[ORM\Column(type: 'string')]
-    private ?string $password = null;
-
-    #[ORM\Column(type: 'string', length: 100, nullable: true)]
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\Length(max: 255)]
     private ?string $nom = null;
 
-    #[ORM\Column(type: 'string', length: 100, nullable: true)]
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\Length(max: 255)]
     private ?string $prenom = null;
+
+    #[ORM\Column(length: 180, unique: true)]
+    #[Assert\NotBlank]
+    #[Assert\Email]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $adresse = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $titrePoste = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $departement = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $equipe = null;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private ?string $message = null;
+
+    #[ORM\Column]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $matricule = null;
+
+    #[ORM\ManyToOne(targetEntity: Role::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    #[Assert\NotNull]
+    private ?Role $role = null;
+
+    #[ORM\Column]
+    private bool $estActif = true;
+
+    #[ORM\Column(type: 'decimal', precision: 5, scale: 2, nullable: true)]
+    #[Assert\GreaterThan(0)]
+    private ?string $capaciteHebdoH = null;
+
+    #[ORM\ManyToMany(targetEntity: Competence::class, inversedBy: 'utilisateurs')]
+    private Collection $competences;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $photoProfil = null;
+
+    #[ORM\ManyToOne(targetEntity: self::class)]
+    private ?self $manager = null;
+
+    #[ORM\Column]
+    private \DateTimeImmutable $dateCreation;
+
+    #[ORM\OneToMany(mappedBy: 'createur', targetEntity: Tache::class)]
+    private Collection $tachesCrees;
+
+    #[ORM\OneToMany(mappedBy: 'assigne', targetEntity: Tache::class)]
+    private Collection $tachesAssignees;
+
+    public function __construct()
+    {
+        $this->competences = new ArrayCollection();
+        $this->tachesCrees = new ArrayCollection();
+        $this->tachesAssignees = new ArrayCollection();
+        $this->dateCreation = new \DateTimeImmutable();
+    }
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+        return $this;
     }
 
     public function getEmail(): ?string
@@ -45,34 +128,61 @@ class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
     public function setEmail(string $email): self
     {
         $this->email = $email;
-
         return $this;
     }
 
-    public function getUserIdentifier(): string
+    public function getAdresse(): ?string
     {
-        return (string) $this->email;
+        return $this->adresse;
     }
 
-    // For Symfony <5.3 compatibility, keep getUsername as alias
-    public function getUsername(): string
+    public function setAdresse(?string $adresse): self
     {
-        return $this->getUserIdentifier();
+        $this->adresse = $adresse;
+        return $this;
     }
 
-    public function getRoles(): array
+    public function getTitrePoste(): ?string
     {
-        $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
-
-        return array_unique($roles);
+        return $this->titrePoste;
     }
 
-    public function setRoles(array $roles): self
+    public function setTitrePoste(?string $titrePoste): self
     {
-        $this->roles = $roles;
+        $this->titrePoste = $titrePoste;
+        return $this;
+    }
 
+    public function getDepartement(): ?string
+    {
+        return $this->departement;
+    }
+
+    public function setDepartement(?string $departement): self
+    {
+        $this->departement = $departement;
+        return $this;
+    }
+
+    public function getEquipe(): ?string
+    {
+        return $this->equipe;
+    }
+
+    public function setEquipe(?string $equipe): self
+    {
+        $this->equipe = $equipe;
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(?string $message): self
+    {
+        $this->message = $message;
         return $this;
     }
 
@@ -84,36 +194,164 @@ class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
     public function setPassword(string $password): self
     {
         $this->password = $password;
-
         return $this;
     }
 
-    public function eraseCredentials()
+    public function getMatricule(): ?string
     {
-        // If you store any temporary, sensitive data on the user, clear it here
+        return $this->matricule;
     }
 
-    public function getNom(): ?string
+    public function setMatricule(?string $matricule): self
     {
-        return $this->nom;
-    }
-
-    public function setNom(?string $nom): self
-    {
-        $this->nom = $nom;
-
+        $this->matricule = $matricule;
         return $this;
     }
 
-    public function getPrenom(): ?string
+    public function getRole(): ?Role
     {
-        return $this->prenom;
+        return $this->role;
     }
 
-    public function setPrenom(?string $prenom): self
+    public function setRole(?Role $role): self
     {
-        $this->prenom = $prenom;
-
+        $this->role = $role;
         return $this;
+    }
+
+    public function isEstActif(): bool
+    {
+        return $this->estActif;
+    }
+
+    public function setEstActif(bool $estActif): self
+    {
+        $this->estActif = $estActif;
+        return $this;
+    }
+
+    public function getCapaciteHebdoH(): ?string
+    {
+        return $this->capaciteHebdoH;
+    }
+
+    public function setCapaciteHebdoH(?string $capaciteHebdoH): self
+    {
+        $this->capaciteHebdoH = $capaciteHebdoH;
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Competence>
+     */
+    public function getCompetences(): Collection
+    {
+        return $this->competences;
+    }
+
+    public function addCompetence(Competence $competence): self
+    {
+        if (!$this->competences->contains($competence)) {
+            $this->competences->add($competence);
+        }
+        return $this;
+    }
+
+    public function removeCompetence(Competence $competence): self
+    {
+        $this->competences->removeElement($competence);
+        return $this;
+    }
+
+    public function getPhotoProfil(): ?string
+    {
+        return $this->photoProfil;
+    }
+
+    public function setPhotoProfil(?string $photoProfil): self
+    {
+        $this->photoProfil = $photoProfil;
+        return $this;
+    }
+
+    public function getManager(): ?self
+    {
+        return $this->manager;
+    }
+
+    public function setManager(?self $manager): self
+    {
+        $this->manager = $manager;
+        return $this;
+    }
+
+    public function getDateCreation(): \DateTimeImmutable
+    {
+        return $this->dateCreation;
+    }
+
+    public function setDateCreation(\DateTimeImmutable $dateCreation): self
+    {
+        $this->dateCreation = $dateCreation;
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Tache>
+     */
+    public function getTachesCrees(): Collection
+    {
+        return $this->tachesCrees;
+    }
+
+    public function addTacheCree(Tache $tache): self
+    {
+        if (!$this->tachesCrees->contains($tache)) {
+            $this->tachesCrees->add($tache);
+            $tache->setCreateur($this);
+        }
+        return $this;
+    }
+
+    public function removeTacheCree(Tache $tache): self
+    {
+        if ($this->tachesCrees->removeElement($tache)) {
+            if ($tache->getCreateur() === $this) {
+                $tache->setCreateur(null);
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Tache>
+     */
+    public function getTachesAssignees(): Collection
+    {
+        return $this->tachesAssignees;
+    }
+
+    // Interface UserInterface
+
+    public function getUserIdentifier(): string
+    {
+        return $this->email;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->email;
+    }
+
+    public function getRoles(): array
+    {
+        // Vous pouvez mapper Role::nomRole vers un rôle Symfony si besoin
+        // Ex: 'ROLE_CHEF_PROJET', 'ROLE_MEMBRE', etc.
+        return ['ROLE_USER'];
+    }
+
+    public function eraseCredentials(): void
+    {
+        // Ne pas stocker de données sensibles en mémoire
     }
 }
