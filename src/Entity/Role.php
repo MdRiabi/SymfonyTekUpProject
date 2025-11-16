2b530772b6d7cc4814e358a97681e17262434672
MdRiabi <mdriabi@gmail.com>
2025-11-16 04:07:18 +0100

    modification des entités role et competence

---
+++ src/Entity/Role.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity]
+class Role
+{
+    #[ORM\Id, ORM\GeneratedValue, ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    #[Assert\NotBlank]
+    private ?string $nomRole = null;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private ?string $description = null;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomRole(): ?string
+    {
+        return $this->nomRole;
+    }
+
+    public function setNomRole(string $nomRole): self
+    {
+        $this->nomRole = $nomRole;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
