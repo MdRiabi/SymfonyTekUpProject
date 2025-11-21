7a0efa857314f9586fde2b35a1e61c6790c405fb
MdRiabi <mdriabi@gmail.com>
2025-11-21 07:13:53 +0100

    Add admin user management with user listing, creation, and request approval.

--- src/Controller/Admin/AdminUserManagementController.php
+++ src/Controller/Admin/AdminUserManagementController.php
@@ -3,12 +3,15 @@
 use App\Repository\UtilisateurRepository;
 use App\Repository\RoleRepository;
 use App\Repository\AccountRequestRepository;
+use App\Form\Admin\UserCreationType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Utilisateur;
 use App\Entity\AccountRequest;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
@@ -22,6 +25,39 @@ class AdminUserManagementController extends AbstractController
 
         return $this->render('admin/users/list.html.twig', [
             'users' => $users,
+        ]);
+    }
+
+    #[Route('/admin/users/create', name: 'admin_create_user')]
+    public function createUser(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $passwordHasher
+    ): Response {
+        $user = new Utilisateur();
+        $form = $this->createForm(UserCreationType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Hash the password
+            $plainPassword = $form->get('plainPassword')->getData();
+            $hashedPassword = $passwordHasher->hashPassword($user, $plainPassword);
+            $user->setPassword($hashedPassword);
+
+            // Set creation date
+            $user->setDateCreation(new \DateTimeImmutable());
+
+            // Persist and flush
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Utilisateur créé avec succès !');
+
+            return $this->redirectToRoute('admin_manage_users');
+        }
+
+        return $this->render('admin/users/createUser.html.twig', [
+            'userCreationForm' => $form->createView(),
         ]);
     }
 
